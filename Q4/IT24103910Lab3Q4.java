4832d44e1599377d441a515bb9ca3ee4114643c9
IT24103910 <it24103910@my.sliit.lk>
2024-11-17 23:54:02 +0530

    Add IT24103910Lab3Q4.java to Q4

---
+++ Q4/IT24103910Lab3Q4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class IT24103910Lab3Q4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a five-digit number: ");
+        
+		int number = scanner.nextInt();
+
+        if (number >= 10000 && number <= 99999) {
+            String numberString = Integer.toString(number);
+            for (int i = 0; i < numberString.length(); i++) {
+                System.out.print(numberString.charAt(i) + " ");
+            }
+        } else {
+            System.out.println("The entered number is not a five-digit number.");
+        }
+    }
+}
